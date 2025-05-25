5d8b4214c13e342c1668325e9a73ab021e1b310c
SHIV167 <jhashiv5@gmail.com>
2025-05-26 05:26:16 +0530

    fix: Database Backup port related updates

--- server/index.ts
+++ server/index.ts
@@ -237,10 +237,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
+  // Serve the app on the port provided by the environment (Render requires dynamic port binding) or fallback to 5000
+  const port = parseInt(process.env.PORT as string, 10) || 5000;
   server.listen({
     port,
     host: "0.0.0.0",
